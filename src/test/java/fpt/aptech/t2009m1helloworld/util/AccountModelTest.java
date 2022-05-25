cc0dd295595ff126cc124201602360e9d928bc3f
nta228 <64982003+nta228@users.noreply.github.com>
2022-05-25 20:20:41 +0700

    Create AccountModelTest.java

---
+++ src/test/java/fpt/aptech/t2009m1helloworld/util/AccountModelTest.java
@@ -0,0 +1,28 @@
+package fpt.aptech.t2009m1baitap2.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountModelTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
